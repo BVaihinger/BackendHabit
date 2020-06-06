7a59dffaf8f42fcf1b7e7e8ef10e9a774772193b
BVaihinger <bvaihinger@colgate.edu>
2020-06-06 16:07:21 -0400

    Initial commit

---
+++ src/main/java/com/bryan/spring/BackendHabitApplication.java
@@ -0,0 +1,13 @@
+package com.bryan.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHabitApplication.class, args);
+	}
+
+}
